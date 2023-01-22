2efe485aa8c59d1e56f544bdb38bf5aa26edc19b
nazarpunk <nazarpunk@gmail.com>
2023-01-22 07:30:40 +0200

    tesh_keywords.db long name fix

--- utils/parse.js
+++ utils/parse.js
@@ -5,14 +5,14 @@ import Database from 'better-sqlite3';
 import parse, {BinaryOp, Call, EmptyLine, FourCC, Globals, Native, Type, Variable} from 'jass-to-ast';
 
 const alias = {
-	'end': 'ends'
+    'end': 'ends'
 };
 
 const _raw2s = string =>
-	string.charCodeAt(3) |
-	string.charCodeAt(2) << 8 |
-	string.charCodeAt(1) << 16 |
-	string.charCodeAt(0) << 24;
+    string.charCodeAt(3) |
+    string.charCodeAt(2) << 8 |
+    string.charCodeAt(1) << 16 |
+    string.charCodeAt(0) << 24;
 
 const _isPrimitive = node => node instanceof String || ['boolean', 'number', 'string'].indexOf(typeof node) >= 0;
 
@@ -23,10 +23,10 @@ const _isPrimitive = node => node instanceof String || ['boolean', 'number', 'st
  * @private
  */
 const _primitive = (s, rawcode = true) => {
-	if (s instanceof FourCC) {
-		return rawcode ? `FourCC('${s}'--[[${_raw2s(s)}--]])` : `${_raw2s(s)}--[[${s}--]]`;
-	}
-	return s;
+    if (s instanceof FourCC) {
+        return rawcode ? `FourCC('${s}'--[[${_raw2s(s)}--]])` : `${_raw2s(s)}--[[${s}--]]`;
+    }
+    return s;
 };
 
 /**
@@ -36,14 +36,14 @@ const _primitive = (s, rawcode = true) => {
  * @private
  */
 const _arg = (node, rawcode = true) => {
-	if (_isPrimitive(node)) {
-		return _primitive(node, rawcode);
-	}
-	if (node instanceof BinaryOp) {
-		return `${node.left}${node.operator}${node.right}`;
-	}
+    if (_isPrimitive(node)) {
+        return _primitive(node, rawcode);
+    }
+    if (node instanceof BinaryOp) {
+        return `${node.left}${node.operator}${node.right}`;
+    }
 
-	return node;
+    return node;
 };
 
 /**
@@ -53,21 +53,21 @@ const _arg = (node, rawcode = true) => {
  * @private
  */
 const _value = (node, rawcode = true) => {
-	if (_isPrimitive(node)) {
-		return _primitive(node, rawcode);
-	}
-	if (node instanceof Call) {
-		const args = [];
-		if (node.args) {
-			for (const arg of node.args) {
-				args.push(_arg(arg, rawcode));
-			}
-		}
-		return `${node.name}(${args.join(', ')})`;
-	}
+    if (_isPrimitive(node)) {
+        return _primitive(node, rawcode);
+    }
+    if (node instanceof Call) {
+        const args = [];
+        if (node.args) {
+            for (const arg of node.args) {
+                args.push(_arg(arg, rawcode));
+            }
+        }
+        return `${node.name}(${args.join(', ')})`;
+    }
 
-	console.log('_value:', node);
-	return node;
+    console.log('_value:', node);
+    return node;
 };
 
 const typeFixPath = './../asset/ConvertTypeFix.lua';
@@ -81,10 +81,27 @@ const techPath = './../jngp/tesh/data/tesh_keywords.db';
 fs.copyFileSync('./../jngp/default/tesh/data/tesh_keywords.db', techPath);
 
 const db = new Database(techPath, {
-	fileMustExist: true,
-	verbose: console.log,
+    fileMustExist: true,
+    verbose: console.log,
 });
-const insert = db.prepare('insert into keywords (keyword_type, name, calltip, description) values (@type, @name, @calltip, @description)');
+const st = db.prepare('insert into keywords (keyword_type, name, calltip, description) values (@type, @name, @calltip, @description)');
+
+const error = [];
+
+const insert = (type, name, calltip, description) => {
+    const mx = 49;
+    if (name.length > mx) {
+        error.push(`(${name.length})${name}`);
+        name = name.substring(0, mx);
+    }
+    return st.run({
+            type: type,
+            name: name,
+            calltip: calltip,
+            description: description
+        }
+    );
+}
 
 const ujapiMap = {};
 
@@ -93,171 +110,162 @@ const ujapiMap = {};
  * @param {boolean} ujapi
  */
 const tolua = (path, {ujapi = false} = {}) => {
-	const ast = parse(fs.readFileSync(path, {encoding: 'utf8', flag: 'r'}));
-	if (!ujapi) {
-		fs.writeFileSync(`${path}.lua`, '', {flag: 'w+'});
-	}
+    const ast = parse(fs.readFileSync(path, {encoding: 'utf8', flag: 'r'}));
+    if (!ujapi) {
+        fs.writeFileSync(`${path}.lua`, '', {flag: 'w+'});
+    }
 
-	/** @param {string} content */
-	const write = content => ujapi ? null : fs.writeFileSync(`${path}.lua`, content, {flag: 'a+'});
+    /** @param {string} content */
+    const write = content => ujapi ? null : fs.writeFileSync(`${path}.lua`, content, {flag: 'a+'});
 
-	/**
-	 * @param node
-	 * @return boolean
-	 */
-	const convert = node => {
-		let s = '';
+    /**
+     * @param node
+     * @return boolean
+     */
+    const convert = node => {
+        let s = '';
 
-		if (node instanceof EmptyLine) {
-			write(`\n`);
-			return true;
-		}
+        if (node instanceof EmptyLine) {
+            write(`\n`);
+            return true;
+        }
 
-		if (node instanceof String) {
-			write(`-- ${node.trim()}\n`);
-			return true;
-		}
+        if (node instanceof String) {
+            write(`-- ${node.trim()}\n`);
+            return true;
+        }
 
-		/** Variable */
-		if (node instanceof Variable) {
-			if (ujapi) {
-				if (node.constant) {
-					insert.run({
-						type: node.name.startsWith('EVENT_') ? 'EVENT' : 'CONSTANT',
-						name: node.name,
-						calltip: '',
-						description: `/*UjAPI*/ constant ${node.type} ${node.name} = ${_value(node.value, false)}`
-					});
-				}
-				return ujapiMap[node.name] = true;
-			}
-			const t = `---@type ${node.type}${ujapiMap[node.name] ? ' @UjAPI' : ''}`;
-			write(`${node.name} = ${_value(node.value)} ${t}\n`);
-			if (node.value instanceof Call) {
-				typeFixWrite(`${node.name} = ${_value(node.value, false)} ${t}\n`);
-			}
-			return true;
-		}
+        /** Variable */
+        if (node instanceof Variable) {
+            if (ujapi) {
+                if (node.constant) {
+                    insert(
+                        node.name.startsWith('EVENT_') ? 'EVENT' : 'CONSTANT',
+                        node.name,
+                        node.name,
+                        `/*UjAPI*/ constant ${node.type} ${node.name} = ${_value(node.value, false)}`
+                    );
+                }
+                return ujapiMap[node.name] = true;
+            }
+            const t = `---@type ${node.type}${ujapiMap[node.name] ? ' @UjAPI' : ''}`;
+            write(`${node.name} = ${_value(node.value)} ${t}\n`);
+            if (node.value instanceof Call) {
+                typeFixWrite(`${node.name} = ${_value(node.value, false)} ${t}\n`);
+            }
+            return true;
+        }
 
-		/** Type */
-		if (node instanceof Type) {
-			if (ujapi) {
-				let description = `/*UjAPI*/ type ${node.base} extends ${node.super}`;
-				if (node.comment) {
-					description += ` //${node.comment.trim()}`;
-				}
-				insert.run({
-					type: 'NATIVE_TYPE',
-					name: node.base,
-					calltip: '',
-					description: description
-				});
+        /** Type */
+        if (node instanceof Type) {
+            if (ujapi) {
+                let description = `/*UjAPI*/ type ${node.base} extends ${node.super}`;
+                if (node.comment) {
+                    description += ` //${node.comment.trim()}`;
+                }
+                insert('NATIVE_TYPE', node.base, '', description);
 
-				return ujapiMap[node.base] = true;
-			}
-			s = `---@class ${node.base}:${node.super}`;
-			const list = [];
-			if (ujapiMap[node.base]) {
-				list.push('UjAPI');
-			}
-			if (node.comment) {
-				list.push(node.comment.trim());
-			}
-			if (list.length) {
-				s += ` @${list.join(' ')}`;
-			}
-			write(`${s.trim()}\n`);
-			return true;
-		}
+                return ujapiMap[node.base] = true;
+            }
+            s = `---@class ${node.base}:${node.super}`;
+            const list = [];
+            if (ujapiMap[node.base]) {
+                list.push('UjAPI');
+            }
+            if (node.comment) {
+                list.push(node.comment.trim());
+            }
+            if (list.length) {
+                s += ` @${list.join(' ')}`;
+            }
+            write(`${s.trim()}\n`);
+            return true;
+        }
 
-		/** Native */
-		if (node instanceof Native) {
-			if (ujapi) {
-				let calltip = '';
-				let description = `${node.constant ? 'constant ' : ''}native ${node.name} takes `;
-				if (node.params) {
-					const list = [];
-					for (const p of node.params) {
-						list.push(`${p.type} ${p.name}`);
-					}
-					const join = list.join(', ');
-					calltip += `(${join})`;
-					description += join;
-				} else {
-					calltip = '(nothing)';
-					description += 'nothing';
-				}
-				const returns = ` returns ${node.returns ?? 'nothing'}`;
-				calltip += returns;
-				description += returns;
+        /** Native */
+        if (node instanceof Native) {
+            if (ujapi) {
+                let calltip = '';
+                let description = `/*UjAPI*/ ${node.constant ? 'constant ' : ''}native ${node.name} takes `;
+                if (node.params) {
+                    const list = [];
+                    for (const p of node.params) {
+                        list.push(`${p.type} ${p.name}`);
+                    }
+                    const join = list.join(', ');
+                    calltip += `(${join})`;
+                    description += join;
+                } else {
+                    calltip = '(nothing)';
+                    description += 'nothing';
+                }
+                const returns = ` returns ${node.returns ?? 'nothing'}`;
+                calltip += returns;
+                description += returns;
 
-				insert.run({
-					type: 'NATIVE_FUNCTION',
-					name: node.name,
-					calltip: calltip,
-					description: description
-				});
-				return ujapiMap[node.name] = true;
-			}
-			if (ujapiMap[node.name]) {
-				write(`---@author UjAPI\n`);
-			}
-			if (node.params) {
-				for (const p of node.params) {
-					if (alias[p.name]) {
-						p.name = alias[p.name];
-					}
-					write(`---@param ${p.name} ${p.type}\n`);
-				}
-			}
-			if (node.returns) {
-				write(`---@return ${node.returns}\n`);
-			}
-			write(`function ${node.name} (`);
-			if (node.params) {
-				const list = [];
-				for (const p of node.params) {
-					list.push(p.name);
-				}
-				if (list.length) {
-					write(list.join(', '));
-				}
-			}
-			write(') end\n');
-			return true;
-		}
+                insert('NATIVE_FUNCTION', node.name, calltip, description);
+                return ujapiMap[node.name] = true;
+            }
+            if (ujapiMap[node.name]) {
+                write(`---@author UjAPI\n`);
+            }
+            if (node.params) {
+                for (const p of node.params) {
+                    if (alias[p.name]) {
+                        p.name = alias[p.name];
+                    }
+                    write(`---@param ${p.name} ${p.type}\n`);
+                }
+            }
+            if (node.returns) {
+                write(`---@return ${node.returns}\n`);
+            }
+            write(`function ${node.name} (`);
+            if (node.params) {
+                const list = [];
+                for (const p of node.params) {
+                    list.push(p.name);
+                }
+                if (list.length) {
+                    write(list.join(', '));
+                }
+            }
+            write(') end\n');
+            return true;
+        }
 
-		console.log(node);
-		return false;
-	};
+        console.log(node);
+        return false;
+    };
 
-	for (const node of ast) {
-		if (node instanceof Globals) {
-			if (node.globals) {
-				for (const global of node.globals) {
-					if (!convert(global)) {
-						break;
-					}
-				}
-			}
-			continue;
-		}
+    for (const node of ast) {
+        if (node instanceof Globals) {
+            if (node.globals) {
+                for (const global of node.globals) {
+                    if (!convert(global)) {
+                        break;
+                    }
+                }
+            }
+            continue;
+        }
 
-		if (!convert(node)) {
-			break;
-		}
-	}
+        if (!convert(node)) {
+            break;
+        }
+    }
 };
 
 tolua('./../sdk/UjAPI.j', {ujapi: true});
 tolua('./../sdk/common.j');
-
+db.close();
+console.log(error);
 
 // noinspection JSUnusedLocalSymbols
 const intToString = number =>
-	String.fromCharCode(
-		number >> 24 & 0xff,
-		number >> 16 & 0xff,
-		number >> 8 & 0xff,
-		number & 0xff
-	);
+    String.fromCharCode(
+        number >> 24 & 0xff,
+        number >> 16 & 0xff,
+        number >> 8 & 0xff,
+        number & 0xff
+    );
