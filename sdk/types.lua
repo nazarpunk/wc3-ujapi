129ab91a4a08f064824938c176ff39fbfca5cec1
nazarpunk <nazarpunk@gmail.com>
2023-02-05 19:03:00 +0200

    add lua added func

--- sdk/types.lua
+++ sdk/types.lua
@@ -3,9 +3,25 @@
 ---@class integer:number
 ---@class code:function
 
+-- https://github.com/Ev3nt/war3_lua/blob/763c5d3b04dc6db60cad9ed27ab3e8e588b67581/Src/LuaFunctions.cpp#L751
+
 ---@param raw string
 ---@return integer
 function FourCC(raw) end
+
+---@param id integer
+---@return string
+function IdToString(id) end
+
+---@param s string
+---@return integer
+function StringToId(s) end
+
+---@return boolean
+function IsDevMode() end
+
+---@return boolean
+function IsUjAPI() end
 
 function printc(...) end
 
